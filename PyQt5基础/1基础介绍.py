65c392f7274a82d24f827378f3f5b27df8edf9e0
lightmumu3 <59609042+lightmumu3@users.noreply.github.com>
2020-06-17 10:53:47 +0800

    Update 1基础介绍.py
    第一课，sys 和 QApplication

--- PyQt5基础/1基础介绍.py
+++ PyQt5基础/1基础介绍.py
@@ -5,12 +5,12 @@ import  sys
 from  PyQt5.QtWidgets import QApplication ,QLabel
 
 if __name__=='__main__':
-    app = QApplication(sys.argv)   # 实例化Qapplication,传入sys.argv参数
-    # label = QLabel('学习第一课6.15')  #实例化控件，也可以后面传递参数
+    app = QApplication(sys.argv)                         # 1. 实例化Qapplication,传入sys.argv参数
+    # label = QLabel('学习第一课6.15')                    # 2. 实例化控件，也可以后面传递参数
     label = QLabel()
     label.setText('<font color = "red">Hello</font> <h1>World</h1>')
-    label.show()                        #默认控件隐藏，这里让他显示
-    sys.exit(app.exec())           #参数是执行应用让他循环,sys.exit()是退出
+    label.show()                                         # 3. 默认控件隐藏，这里让他显示
+    sys.exit(app.exec())                                 # 4. 参数是执行应用让他循环,sys.exit()是退出
 
 
 
